77de083ce65734f004a6df5b3073006a73b8f36c
JaeHyun Jeong <jjgus29@naver.com>
2024-10-27 17:26:48 +0900

    first

---
+++ java_jungsuk/src/VarEx2.java
@@ -0,0 +1,17 @@
+//변수 swap
+
+public class VarEx2 {
+    public static void main(String[] agrs) {
+        int x = 10, y = 20;
+        int tmp = 0;
+
+        System.out.println("x:" + x + "\ny:" + y);
+
+        tmp = x;
+        x = y;
+        y = tmp;
+
+        System.out.println("\nafter\nx:" + x + "\ny:" + y);
+
+    }
+}
